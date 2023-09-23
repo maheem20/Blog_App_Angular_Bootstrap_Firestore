052951d812a10ad40ea718e4eab6f7c14b51027f
maheem_20 <mwistanbuly@gmail.com>
2023-09-23 23:50:26 +0200

    create comment-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { SubsciptionFormComponent } from './subsciption-form/subsciption-form.component';
 import { CommentFormComponent } from './comments/comment-form/comment-form.component';
+import { CommentListComponent } from './comments/comment-list/comment-list.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { CommentFormComponent } from './comments/comment-form/comment-form.compo
     TermsAndConditionsComponent,
     ContactUsComponent,
     SubsciptionFormComponent,
-    CommentFormComponent
+    CommentFormComponent,
+    CommentListComponent
   ],
   imports: [
     BrowserModule,
