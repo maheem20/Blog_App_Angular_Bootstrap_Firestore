1b80722e3b383257fb6918030f1059060ae0ef76
maheem_20 <mwistanbuly@gmail.com>
2023-09-24 13:32:58 +0200

    import about-us component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { SingleCategoryComponent } from './pages/single-category/single-category
 import { SinglePostComponent } from './pages/single-post/single-post.component';
 import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'category', component: SingleCategoryComponent },
   { path: 'post', component: SinglePostComponent },
 
+  { path: 'about', component: AboutUsComponent },
   { path: 'terms', component: TermsAndConditionsComponent },
   { path: 'contact', component: ContactUsComponent },
 ];
