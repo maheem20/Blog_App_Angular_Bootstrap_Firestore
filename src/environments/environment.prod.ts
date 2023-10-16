e086bca9d9ea7b14e65eee43c00db4abb4b1bbf9
maheem_20 <mwistanbuly@gmail.com>
2023-10-16 22:42:21 +0200

    define Firebase credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
     production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyDjmwsEQ2KLW4O3ELFhp0QIsNVp79R8Iw0",
+        authDomain: "angular-blog-b07da.firebaseapp.com",
+        projectId: "angular-blog-b07da",
+        storageBucket: "angular-blog-b07da.appspot.com",
+        messagingSenderId: "153621353491",
+        appId: "1:153621353491:web:77339089920112b50f1be6"
+    }
 };
