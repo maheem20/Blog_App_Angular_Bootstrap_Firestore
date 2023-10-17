be90ca56a2d1b8830386ff9d8dc8616cd376903b
maheem_20 <mwistanbuly@gmail.com>
2023-10-17 21:33:33 +0200

    inject AngularFirestore into constructor

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
 }
