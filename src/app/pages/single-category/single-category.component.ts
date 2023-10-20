921fadf5c091b936881bf71b388868d3902c38db
Maheem <88165826+maheem20@users.noreply.github.com>
2023-10-20 21:42:04 +0000

    import & implement OnInit

--- src/app/pages/single-category/single-category.component.ts
+++ src/app/pages/single-category/single-category.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-category',
   templateUrl: './single-category.component.html',
   styleUrls: ['./single-category.component.css']
 })
-export class SingleCategoryComponent {
+export class SingleCategoryComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
 }
