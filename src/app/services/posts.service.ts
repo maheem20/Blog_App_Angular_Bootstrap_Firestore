3f92970931cc648505eded20d45a48bb3a41e96f
maheem_20 <mwistanbuly@gmail.com>
2023-11-04 23:58:36 +0100

    function to increment view count of post

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { map } from 'rxjs';
 
+import * as firebase from 'firebase/compat/app';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -35,16 +37,16 @@ export class PostsService {
 
   loadCategoryPosts(categoryId: string) {
     return this.afs.collection('posts', ref => ref.where('categoryId', '==', categoryId).limit(4))
-    .snapshotChanges().pipe(
-      map(
-        actions => {
-        return actions.map(a => {
-          const data: any = a.payload.doc.data();
-          const id = a.payload.doc.id;
-          return { id, ...data };
-        });
-      })
-    );
+      .snapshotChanges().pipe(
+        map(
+          actions => {
+            return actions.map(a => {
+              const data: any = a.payload.doc.data();
+              const id = a.payload.doc.id;
+              return { id, ...data };
+            });
+          })
+      );
   }
 
   loadOnePost(postId: any) {
@@ -53,16 +55,27 @@ export class PostsService {
 
   loadSimilar(categoryId: any) {
     return this.afs.collection('posts', ref => ref.where('category.categoryId', '==', categoryId).limit(4))
-    .snapshotChanges().pipe(
-      map(
-        actions => {
-        return actions.map(a => {
-          const data: any = a.payload.doc.data();
-          const id = a.payload.doc.id;
-          return { id, ...data };
-        });
-      })
-    );
+      .snapshotChanges().pipe(
+        map(
+          actions => {
+            return actions.map(a => {
+              const data: any = a.payload.doc.data();
+              const id = a.payload.doc.id;
+              return { id, ...data };
+            });
+          })
+      );
+  }
+
+  countViews(postId: any) {
+
+    const viewsCount = {
+      views: firebase.default.firestore.FieldValue.increment(1)
+    }
+
+    this.afs.doc(`posts/${postId}`).update(viewsCount).then(() => {
+      console.log('Views count updated');
+    });
   }
 
 }
