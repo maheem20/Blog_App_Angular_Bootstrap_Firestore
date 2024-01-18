051fc33e148dd30d720016f97b5f5280add7f739
maheem_20 <mwistanbuly@gmail.com>
2024-01-18 23:19:07 +0100

    add OnInit interface and SubscribersService

--- src/app/subscription-form/subscription-form.component.ts
+++ src/app/subscription-form/subscription-form.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Sub } from '../models/sub';
+import { SubscribersService } from '../services/subscribers.service';
 
 @Component({
   selector: 'app-subscription-form',
   templateUrl: './subscription-form.component.html',
   styleUrls: ['./subscription-form.component.css']
 })
-export class SubscriptionFormComponent {
+export class SubscriptionFormComponent implements OnInit {
+
+  constructor(private subService: SubscribersService) { }
+
+  ngOnInit(): void {
+  }
 
   onSubmit(formVal: any) {
     const subData: Sub = {
